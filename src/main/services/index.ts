ee48e25366ffbdd1e38af5e85340e7bf39b6e56a
Kishor <kishor@sunai.xyz>
2022-08-16 16:10:45 +0545

    added axios instance

--- src/main/services/index.ts
+++ src/main/services/index.ts
@@ -1,1 +1,33 @@
-export {};
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import store from "store";
+
+const baseUrl = process.env.REACT_APP_API_URL;
+
+const http = axios.create({
+  baseURL: baseUrl,
+});
+
+//get the access token set in reducer
+http.interceptors.request.use((_req: AxiosRequestConfig) => {
+  const { access_token } = store.getState().user;
+  _req.headers = {
+    ..._req.headers,
+    Authorization: `Bearer ${access_token} `,
+  };
+  return _req;
+});
+
+/**
+ * TODO: show toast on request failutre
+ */
+http.interceptors.response.use((_res: AxiosResponse) => {
+  return _res;
+});
+/**
+ * TODO: handle timeout
+ */
+http.interceptors.response.use((_res: AxiosResponse) => {
+  return _res;
+});
+
+export default http;
--- src/main/store/reducers/user.ts
+++ src/main/store/reducers/user.ts
@@ -2,12 +2,21 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 const initialState = {
   name: "",
+  access_token: "",
 };
 
 export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
+    /**
+     * Function to initialzate user token
+     * @param state
+     * @param action
+     */
+    initToken: (state, action: PayloadAction<string>) => {
+      state.access_token = action.payload;
+    },
     updateName: (state, action: PayloadAction<string>) => {
       state.name = action.payload;
     },
