9194997f136ddb96f052e56c52b19488edca41a4
Kishor <kishor@sunai.xyz>
2022-08-23 14:47:13 +0545

    static sidebar item rendering completed

--- src/lib/layouts/main-layout.tsx
+++ src/lib/layouts/main-layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar/Sidebar";
 
-const MainLayout   = () => {
+const MainLayout = () => {
   return (
     <>
-      <Outlet />
+      <Sidebar />
+      {/* <Outlet /> */}
     </>
   );
 };
